efc9e3404c4bbe2b7151996ffae8a2ed6d00c186
arbrown110 <arbrown110@gmail.com>
2020-09-27 03:19:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const submitData = function(name, email) {
+
+    const requestBody = {
+        name: name, 
+        email: email
+    };
+
+    const userForm = document.querySelector("#userForm")
+
+    const  configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+    }; 
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then((response) => response.text())
+        .then((json) => {
+            document.getElementById('output').innerHTML = json;
+            
+        })
+        .catch((error) => {
+            document.body.innerHTML = error.message;
+        })
+}
