272a310818aacad82b93c44bd10f5cda3c23850f
nabilhayet <nabil4457@yahoo.com>
2020-10-04 23:05:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+let formData = {
+    name: "Nabil Hayet",
+    email: "nabil4457@yahoo.com"
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  function submitData(name,email){
+   return fetch("http://localhost:3000/users", configObj);
+   .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    let body = document.querySelector("body")
+    let newLi = document.createElement("li")
+    newLi.innerText = object.id 
+    body.appendChild(newLi)
+  })
+  .catch(function(error) {
+    alert("Unexpected id returned!");
+    console.log(error.message);
+  });
+}
