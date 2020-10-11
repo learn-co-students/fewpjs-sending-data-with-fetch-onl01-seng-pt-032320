228e282b0c6636a5b44764fc6e8b114508cef912
omarnabi1 <onabi20@gmail.com>
2020-10-11 17:24:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+
+function submitData(name,email){
+    let formData = {name,email}
+    const configurationObject = {
+     method: "POST",
+     headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json"
+     },
+     body: JSON.stringify(formData)
+     }
+ 
+     return fetch('http://localhost:3000/users', configurationObject)
+     .then(response => response.json())
+     .then(object => document.body.innerHTML = object[ "id" ])
+     .catch(function(error) {
+         document.body.innerHTML = error.message
+       });
+        
+  
+ }
