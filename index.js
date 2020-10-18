96e706f069b57476cdf2bf603d556b408f62ab07
jhar3ll <jharell.chambers@selu.edu>
2020-10-18 11:01:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Add your code here
+
+/* let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+   
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    }); */
+
+    function submitData(name, email) {
+  
+        let configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                name,
+                email
+            })
+        }
+
+       return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object){
+            document.body.innerHTML = object['id']
+        })
+        .catch(function(error){
+            document.body.innerHTML = error.message
+        })
+    }
